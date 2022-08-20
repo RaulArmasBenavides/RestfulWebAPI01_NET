4ce54fab1fc364f5d1479d33c0786886c90a5cac
Raul Armas <you@example.com>
2022-08-20 09:22:18 -0500

    updates

--- WebAPI/WebAPI/Controllers/ValuesController.cs
+++ WebAPI/WebAPI/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
 
 namespace WebAPI.Controllers
 {
@@ -14,22 +15,39 @@ namespace WebAPI.Controllers
         {
             return new string[] { "value1", "value2" };
         }
-
-        // GET api/values/5
-        public string Get(int id)
+        // GET api/values2
+        public JsonResult Otro()
         {
-            return "value";
+            var result = new JsonResult();
+            result.Data = new
+            {
+                id = 2
+            };
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return result;
         }
 
-        // POST api/values
-        public void Post([FromBody] string value)
+        //// POST api/values
+        public JsonResult ExampleJSON()
         {
+            var result = new JsonResult();
+            result.Data = new
+            {
+                id = 1
+            };
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return result;
         }
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        //// POST api/values
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
         // DELETE api/values/5
         public void Delete(int id)
